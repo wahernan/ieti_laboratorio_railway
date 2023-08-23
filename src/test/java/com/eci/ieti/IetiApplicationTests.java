8f038d0cc33c7243b0fcf5737a26f0618d231c50
Wilson Delgado <wilson.delgadoher@gmail.com>
2023-08-22 20:44:06 -0500

    health endpoint

---
+++ src/test/java/com/eci/ieti/IetiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eci.ieti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IetiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
